c5690fce7973ba8f6ebb067bed21d1669d938598
Simon <buzasi.simon@gmail.com>
2021-09-26 18:40:13 +0200

    create backend base project
    The base solution contains the needed projects and the basic required NuGet packages. The Wman.WebAPI project already has a DemoController too.

---
+++ wman-backend/Wman.WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wman.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
