2ae1cac5066eb74e0dfd268eda58aad4c959c437
Luxeder Zoltán <luxederzoltan@gmail.com>
2021-09-29 12:13:18 +0200

    Created db

---
+++ wman-backend/Wman.Data/DB_Models/WorkEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wman.Data.DB_Models
+{
+    class WorkEvent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string JobDescription { get; set; }
+    }
+}
---
+++ wman-backend/Wman.Data/DbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wman.Data
+{
+    class DbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbContext(DbContextOptions<DbContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\wmandb.mdf;integrated security=True;MultipleActiveResultSets=True");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new { JobDescription = "asd" }
+            );
+        }
+        public virtual DbSet<DB_Models.WorkEvent> OneVote { get; set; }
+
+    }
+}
