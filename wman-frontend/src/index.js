7b7f4b138981929ef48a87a40323106cf68c857a
Szabó Dárius <szabo.dariusz@gmail.com>
2021-10-04 16:17:30 +0200

    login function created

--- wman-frontend/src/index.js
+++ wman-frontend/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { NormalLoginForm } from './components/login.component';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+    <App/>
+    </React.StrictMode>,
+    document.getElementById('root'));
 registerServiceWorker();
