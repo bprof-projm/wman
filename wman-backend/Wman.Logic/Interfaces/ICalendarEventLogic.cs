a59499f3a238bb4dc011c0250166f815fe0ff5b5
Akos Gadacsi <akosgadacsi99@gmail.com>
2021-10-04 20:41:49 +0200

    Calendar event logic interface

---
+++ wman-backend/Wman.Logic/Interfaces/ICalendarEventLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wman.Data.DB_Models;
+
+namespace Wman.Logic.Interfaces
+{
+    public interface ICalendarEventLogic
+    {
+        Task<List<WorkEvent>> GetCurrentWeekEvents();
+        Task<List<WorkEvent>> GetCurrentDayEvents();
+        List<WorkEvent> GetWeekEvents(int week);
+        Task<List<WorkEvent>> GetDayEvents(int day);
+        Task<List<WorkEvent>> GetDayEvents(DateTime day);
+        Task<List<WorkEvent>> GetWeekEvents(DateTime firstDayOfTheWeek, DateTime lastDayOfTheWeek);
+        
+    }
+}
