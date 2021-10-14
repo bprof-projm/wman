12cc1f0a216d7def8b633c45500bf8344e85a489
tubamark98 <val.afo00@gmail.com>
2021-10-14 14:56:03 +0200

    CalendarEventLogicTest base created

--- wman-backend/Wman.Test/Builders/CalendarEventLogicBuilder.cs
+++ wman-backend/Wman.Test/Builders/CalendarEventLogicBuilder.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Wman.Test.Builders
 {
-    class CalendarEventLogicBuilder
+    public class CalendarEventLogicBuilder
     {
     }
 }
--- wman-backend/Wman.Test/Tests/CalendarEventLogicTests.cs
+++ wman-backend/Wman.Test/Tests/CalendarEventLogicTests.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Wman.Data.DB_Models;
+using Wman.Repository.Interfaces;
+using Wman.Test.Builders;
 
 namespace Wman.Test.Tests
 {
-    class CalendarEventLogicTests
+    public class CalendarEventLogicTests
     {
+        private Mock<IWorkEventRepo> eventRepo;
+
+        private List<WorkEvent> eventList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.eventList = EventLogicBuilder.GetWorkEvents();
+
+            this.eventRepo = EventLogicBuilder.GetEventRepo(eventList);
+        }
     }
 }
