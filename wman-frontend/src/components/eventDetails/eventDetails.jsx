80911c8e8c01ecb40fabe9ac9f1d089c50fd68ee
Tamas Lengyel <lengyeltamas97@gmail.com>
2021-10-17 21:48:16 +0200

    Added CSS to Event modifying card.

--- wman-frontend/src/components/eventDetails/eventDetails.jsx
+++ wman-frontend/src/components/eventDetails/eventDetails.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Form, Input, Button, Select, DatePicker } from "antd";
 import "antd/dist/antd.css";
+import "./eventDetails.css";
 
 const { RangePicker } = DatePicker;
 const rangeConfig = {
@@ -11,45 +12,49 @@ const EventDetails = () => {
   const [componentSize, setComponentSize] = useState("default");
 
   return (
-    <Form
-      labelCol={{
-        span: 4,
-      }}
-      wrapperCol={{
-        span: 14,
-      }}
-      layout="horizontal"
-      initialValues={{
-        size: componentSize,
-      }}
-      size={componentSize}
-    >
-      <Form.Item label="Description">
-        <Input />
-      </Form.Item>
+    <div className="card-container">
+      <div className="card">
+        <Form
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 14,
+          }}
+          layout="horizontal"
+          initialValues={{
+            size: componentSize,
+          }}
+          size={componentSize}
+        >
+          <Form.Item label="Description">
+            <Input />
+          </Form.Item>
 
-      <Form.Item label="Workers">
-        <Select>
-          <Select.Option value="demo">Demo</Select.Option>
-        </Select>
-      </Form.Item>
-      <Form.Item label="Labels">
-        <Select>
-          <Select.Option value="demo">Demo</Select.Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        name="range-time-picker"
-        label="RangePicker[showTime]"
-        {...rangeConfig}
-      >
-        <RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-      </Form.Item>
+          <Form.Item label="Workers">
+            <Select>
+              <Select.Option value="demo">Demo</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Labels">
+            <Select>
+              <Select.Option value="demo">Demo</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="range-time-picker"
+            label="RangePicker"
+            {...rangeConfig}
+          >
+            <RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+          </Form.Item>
 
-      <Form.Item label="Button">
-        <Button>Button</Button>
-      </Form.Item>
-    </Form>
+          <Form.Item label="Button">
+            <Button>Button</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 };
 export default EventDetails;
