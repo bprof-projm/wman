1599360832ebe1d4ffceae9f8809412d5f7563a8
Luxeder Zoltán <luxederzoltan@gmail.com>
2021-10-27 22:26:04 +0200

    Created layers for user management related classes

---
+++ wman-backend/Wman.Logic/Classes/UserLogic.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wman.Data.DB_Models;
+using Wman.Logic.Interfaces;
+
+namespace Wman.Logic.Classes
+{
+    public class UserLogic : IUserLogic
+    {
+        UserManager<WmanUser> userManager;
+
+        public UserLogic(UserManager<WmanUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+    }
+}
---
+++ wman-backend/Wman.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wman.Logic.Interfaces;
+
+namespace Wman.WebAPI.Controllers
+{
+    /// <summary>
+    /// UserController
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IUserLogic userLogic;
+
+        public UserController(IUserLogic userLogic)
+        {
+            this.userLogic = userLogic;
+        }
+    }
+}
