2f48a17833e47723c1eeaa6e6ecf77c6a9a56e0e
Luxeder Zoltán <luxederzoltan@gmail.com>
2021-10-27 22:13:27 +0200

    Created Workload dto

---
+++ wman-backend/Wman.Logic/DTO_Models/WorkloadDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wman.Data.DB_Models;
+
+namespace Wman.Logic.DTO_Models
+{
+   public class WorkloadDTO
+    {
+        public string Username { get; set; }
+        public Pictures ProfilePic { get; set; }
+        public int Percent { get; set; }
+    }
+}
