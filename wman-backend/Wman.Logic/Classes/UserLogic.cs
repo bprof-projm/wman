94c31d821d6b96f09a733946eaaf98c08d563d0e
Luxeder Zoltán <luxederzoltan@gmail.com>
2021-10-28 22:30:47 +0200

    Added basic workload calculating logic

--- wman-backend/Wman.Logic/Classes/UserLogic.cs
+++ wman-backend/Wman.Logic/Classes/UserLogic.cs
@@ -17,5 +17,38 @@ namespace Wman.Logic.Classes
         {
             this.userManager = userManager;
         }
+
+        private int calculate(WmanUser user)
+        {
+            var beforeToday = WorksBeforeToday(user.WorkEvents);
+            var fromToday = RemainingWorks(user.WorkEvents);
+            //List<TimeSpan> tsBeforeToday = new List<TimeSpan>();
+            //List<TimeSpan> tsFromToday = new List<TimeSpan>();
+
+            TimeSpan tsBeforeToday = TimeSpan.Zero;
+            TimeSpan tsFromToday = TimeSpan.Zero;
+            
+            foreach (var item in beforeToday)
+            {
+                tsFromToday += (item.WorkFinishDate - item.WorkStartDate);
+            }
+            foreach (var item in fromToday)
+            {
+                tsFromToday += (item.EstimatedFinishDate - item.EstimatedStartDate);
+            }
+
+            TimeSpan tsSUM = tsBeforeToday + tsFromToday;
+            return tsSUM.Hours / 168;
+        }
+        private IEnumerable<WorkEvent> WorksBeforeToday(IEnumerable<WorkEvent> works)
+        {
+            return works.Where(x => x.WorkStartDate.DayOfYear < DateTime.Now.DayOfYear && x.WorkStartDate.Year == DateTime.Now.Year);
+        }
+
+        private IEnumerable<WorkEvent> RemainingWorks(IEnumerable<WorkEvent> works)
+        {
+            return works.Where(x => x.EstimatedStartDate.DayOfYear <= DateTime.Now.DayOfYear && x.EstimatedStartDate.Year == DateTime.Now.Year);
+        }
+
     }
 }
