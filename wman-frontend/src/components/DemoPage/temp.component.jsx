70ae6c5a027e23782641b9749056f4d181cf6fe5
Szabó Dárius <szabo.dariusz@gmail.com>
2021-11-03 18:24:09 +0100

    Add temporary page for demo

---
+++ wman-frontend/src/components/DemoPage/temp.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./temp.styles.css";
+
+export const TempPage = () => {
+    
+    return (
+        <div>
+            <h1>Ideiglenes oldal bemutatáshoz</h1>
+        </div>
+    )
+};
--- wman-frontend/src/index.js
+++ wman-frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { NormalLoginForm } from './components/login.component';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
