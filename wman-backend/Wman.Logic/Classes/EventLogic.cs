ba2f67537fcf3bc7f114a505030c5e9680bcb67d
Luxeder Zoltán <luxederzoltan@gmail.com>
2021-11-07 18:57:08 +0100

    Added role checks to user assign to event logic
    Currently disabled, because in some cases, we might want to assign a non-worker user to an event.

--- wman-backend/Wman.Logic/Classes/EventLogic.cs
+++ wman-backend/Wman.Logic/Classes/EventLogic.cs
@@ -39,6 +39,11 @@ namespace Wman.Logic.Classes
             {
                 throw new ArgumentException("User not found! ");
             }
+            //if (await userManager.IsInRoleAsync(selectedUser, "Worker") == false)
+            //{
+            //    throw new InvalidOperationException("Selected user does not have the provided role.");
+            //}
+
             bool testResult = await this.DoTasksOverlap(selectedUser.WorkEvents, selectedEvent);
             ;
             if (testResult)
@@ -69,7 +74,10 @@ namespace Wman.Logic.Classes
                 {
                     throw new ArgumentException($"User: {0} not found", item);
                 }
-
+                //if (await userManager.IsInRoleAsync(selectedUser, "Worker") == false)
+                //{
+                //    throw new InvalidOperationException(String.Format("User: {0} does not have the provided role.", selectedUser.UserName));
+                //}
                 testresult = await this.DoTasksOverlap(selectedUser.WorkEvents, selectedEvent);
                 if (testresult)
                 {
