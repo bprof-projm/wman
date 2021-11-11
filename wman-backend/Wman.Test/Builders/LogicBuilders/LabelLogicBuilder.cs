c2e072b3f97e7d5412132533d0710592a640a84a
tubamark98 <val.afo00@gmail.com>
2021-11-11 15:42:17 +0100

    LabelLogicTests setup completed

---
+++ wman-backend/Wman.Test/Builders/LogicBuilders/LabelLogicBuilder.cs
@@ -0,0 +1,36 @@
+﻿using MockQueryable.Moq;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Wman.Data.DB_Models;
+using Wman.Repository.Interfaces;
+
+namespace Wman.Test.Builders.LogicBuilders
+{
+    public class LabelLogicBuilder
+    {
+        public static Mock<ILabelRepo> GetLabelRepo(List<Label> labelList)
+        {
+            var labelRepo = new Mock<ILabelRepo>();
+            var mock = labelList.AsQueryable().BuildMock();
+
+            labelRepo.Setup(x => x.GetAll()).Returns(mock.Object);
+            labelRepo.Setup(x => x.GetOne(It.IsAny<int>())).ReturnsAsync(labelList[0]);
+
+            return labelRepo;
+        }
+
+        public static List<Label> GetLabels()
+        {
+            List<Label> labelList = new();
+
+            labelList.Add( new Label{
+                Id = 0,
+                Color = "blue",
+                Content = "something"
+            });
+
+            return labelList;
+        }
+    }
+}
---
+++ wman-backend/Wman.Test/Tests/LabelLogicTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Wman.Data.DB_Models;
+using Wman.Repository.Interfaces;
+using Wman.Test.Builders;
+using Wman.Test.Builders.LogicBuilders;
+
+namespace Wman.Test.Tests
+{
+    public class LabelLogicTests
+    {
+        private Mock<ILabelRepo> labelRepo;
+        private Mock<IWorkEventRepo> eventRepo;
+        private IMapper mapper;
+
+        private List<Label> labelList;
+        private List<WorkEvent> eventList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mapper = MapperBuilder.GetMapper();
+
+            this.labelList = LabelLogicBuilder.GetLabels();
+            this.labelRepo = LabelLogicBuilder.GetLabelRepo(this.labelList);
+
+            this.eventList = EventLogicBuilder.GetWorkEvents();
+            this.eventRepo = EventLogicBuilder.GetEventRepo(this.eventList);
+        }
+    }
+}
