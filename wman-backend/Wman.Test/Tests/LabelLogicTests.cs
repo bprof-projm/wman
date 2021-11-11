205a6fd8f23396d591608d9e5789e352bebf223a
tubamark98 <val.afo00@gmail.com>
2021-11-11 18:59:13 +0100

    CreateLabel tested in LabelLogicTests

--- wman-backend/Wman.Test/Tests/LabelLogicTests.cs
+++ wman-backend/Wman.Test/Tests/LabelLogicTests.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Wman.Data.DB_Models;
 using Wman.Logic.Classes;
+using Wman.Logic.DTO_Models;
 using Wman.Repository.Interfaces;
 using Wman.Test.Builders;
 using Wman.Test.Builders.LogicBuilders;
@@ -45,6 +47,25 @@ namespace Wman.Test.Tests
             //Assert
             Assert.That(call.Count() == labelList.Count());
             this.labelRepo.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Test]
+        public async Task CreateLabel_CreatedNewLabel_AddedToRepoSuccessfully()
+        {
+            //Arrange
+            LabelLogic labelLogic = new LabelLogic(this.labelRepo.Object, this.mapper, this.eventRepo.Object);
+            CreateLabelDTO newLabel = new()
+            {
+                Color= "#000000",
+                Content = "TestCaseMeow"
+            };
+
+            //Act
+            var call = labelLogic.CreateLabel(newLabel);
+
+            //Assert
+            this.labelRepo.Verify(x => x.GetAll(), Times.Once);
+            this.labelRepo.Verify(x => x.Add(It.IsAny<Label>()), Times.Once);
         }
     }
 }
