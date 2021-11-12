7e5e4f3c47b846c1efdea510c10c2af60503195c
Luxeder Zoltán <luxederzoltan@gmail.com>
2021-11-12 13:01:32 +0100

    Wmanerror.cleanup

--- wman-backend/Wman.Logic/Helpers/WmanError.cs
+++ wman-backend/Wman.Logic/Helpers/WmanError.cs
@@ -8,19 +8,19 @@ namespace Wman.Logic.Helpers
 {
     public class WmanError
     {
-        public static string UserNotFound { get { return "The selected user could not be found"; } }
-        public static string IncorrectPassword { get { return "The provided password is incorrect"; } }
-        public static string EmailExists { get { return "There is already a user registered with the provided email address"; } }
-        public static string RoleNotFound { get { return "The specified role does not exist"; } }
-        public static string EventNotFound { get { return "The specified event does not exist"; } }
-        public static string InvalidInputRange { get { return "The provided input parameter is not in the valid range"; } }
-        public static string NotMemberOfRole { get { return "The user '{0}' does not have the role '{1}'"; } }
-        public static string UserIsBusy { get { return "The user is busy in the event's timeframe"; } }
-        public static string EventDateInvalid { get { return "The event date is invalid!"; } } //Not in same day/starts after it finishes
-        public static string LabelExists { get { return "This Label already exists!"; } }
-        public static string NoLabels { get { return "There are no labels added!"; } }
-        public static string LabelNotFound { get { return "Label not found!"; } }
-        public static string WrongColor { get { return "Color is wrong?"; } } //TODO
-        public static string PhotoNotFound { get { return "Photo not found"; } }
+        public static string UserNotFound = "The selected user could not be found";
+        public static string IncorrectPassword = "The provided password is incorrect";
+        public static string EmailExists = "There is already a user registered with the provided email address";
+        public static string RoleNotFound = "The specified role does not exist";
+        public static string EventNotFound = "The specified event does not exist";
+        public static string InvalidInputRange = "The provided input parameter is not in the valid range";
+        public static string NotMemberOfRole = "The user '{0}' does not have the role '{1}'";
+        public static string UserIsBusy = "The user is busy in the event's timeframe";
+        public static string EventDateInvalid = "The event date is invalid!"; //Not in same day/starts after it finishes
+        public static string LabelExists = "This Label already exists!";
+        public static string NoLabels = "There are no labels added!";
+        public static string LabelNotFound = "Label not found!";
+        public static string WrongColor = "Color is wrong?"; //TODO
+        public static string PhotoNotFound = "Photo not found";
     }
 }
