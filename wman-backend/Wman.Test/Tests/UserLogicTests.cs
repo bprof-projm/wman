ca3af3c5551170bc10193fc4e1ab8fbdca2592ed
tubamark98 <val.afo00@gmail.com>
2021-11-14 09:58:56 +0100

    simple GetWorkLoads test in UserLogicTests

--- wman-backend/Wman.Test/Tests/UserLogicTests.cs
+++ wman-backend/Wman.Test/Tests/UserLogicTests.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Wman.Data.DB_Models;
+using Wman.Logic.Classes;
 using Wman.Test.Builders;
+using Wman.Test.Builders.LogicBuilders;
 
 namespace Wman.Test.Tests
 {
@@ -21,9 +23,25 @@ namespace Wman.Test.Tests
         public void SetUp()
         {
             this.users = UserManagerBuilder.GetWmanUsers();
-            this.userManager = UserManagerBuilder.GetUserManager(users);
+            UserLogicBuilder.AssignWorkEvents(this.users);
+            this.userManager = UserManagerBuilder.GetUserManager(this.users);
 
             this.mapper = MapperBuilder.GetMapper();
+        }
+
+        [Test]
+        public async Task GetWorkLoads_ReturnsRepoProperly_SuccessfulOperation()
+        {
+            //Arrange
+            UserLogic userLogic = new (this.userManager.Object, this.mapper);
+
+            //Act
+            var call = await userLogic.GetWorkLoads();
+
+            //Assert
+            Assert.That(call.ElementAt(2).Username == users[2].UserName);
+            this.userManager.Verify(x => x.Users, Times.Once);
+            this.userManager.Verify(x => x.IsInRoleAsync(It.IsAny<WmanUser>(), It.IsAny<string>()), Times.Exactly(users.Count()));
         }
     }
 }
