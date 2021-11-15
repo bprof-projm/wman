0a304b7d976050b8471ac52ac233a370ed286505
Akos Gadacsi <BL3F7U@stud.uni-obuda.hu>
2021-11-15 16:46:11 +0100

    srry that I forgot the 2nd part of the issue
    I used 3 foreach, pls dont throw stones at me

--- wman-backend/Wman.Logic/Classes/AllInWorkEventLogic.cs
+++ wman-backend/Wman.Logic/Classes/AllInWorkEventLogic.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +15,14 @@ namespace Wman.Logic.Classes
 {
     public class AllInWorkEventLogic : IAllInWorkEventLogic
     {
+        UserManager<WmanUser> userManager;
         IWorkEventRepo workEvent;
         ILabelRepo labelRepo;
         IMapper mapper;
 
-        public AllInWorkEventLogic(IWorkEventRepo workEvent, ILabelRepo labelRepo, IMapper mapper)
+        public AllInWorkEventLogic(UserManager<WmanUser> userManager, IWorkEventRepo workEvent, ILabelRepo labelRepo, IMapper mapper)
         {
+            this.userManager = userManager;
             this.workEvent = workEvent;
             this.labelRepo = labelRepo;
             this.mapper = mapper;
@@ -30,6 +34,71 @@ namespace Wman.Logic.Classes
 
             var result = mapper.Map<WorkEventForWorkCardDTO>(selectedWorkEvent);
             return result;
+        }
+
+        public async Task<List<WorkerDTO>> Available (DateTime fromDate, DateTime toDate)
+        {
+            
+
+            if (fromDate<toDate)
+            {
+                var allUsers = userManager.Users
+                .Include(y => y.WorkEvents)
+                .Include(z => z.ProfilePicture);
+
+                var notavailableWorkers = await (from x in workEvent.GetAll()
+                                              where fromDate <= x.EstimatedStartDate && toDate >= x.EstimatedFinishDate
+                                              select x.AssignedUsers).ToListAsync();
+
+                List<WorkerDTO> workersDTO = new List<WorkerDTO>();
+
+                List<int> notAvailableWorkerID = new List<int>();
+                foreach (var workers in notavailableWorkers)
+                {
+                    foreach (var item in workers)
+                    {
+                        var valami = await userManager.GetRolesAsync(item);
+                        if (valami.FirstOrDefault() == "Worker")
+                        {
+                            notAvailableWorkerID.Add(item.Id);
+                        }
+                    }
+
+                }
+                List<int> workerID = new List<int>();
+                foreach (var item in userManager.Users)
+                {
+                    var valami = await userManager.GetRolesAsync(item);
+                    if (valami.FirstOrDefault() == "Worker")
+                    {
+                        workerID.Add(item.Id);
+                    }   
+                }
+
+                List<int> rightWorkers = new List<int>();
+                foreach (var item in workerID)
+                {
+                    if (!notAvailableWorkerID.Any(x => x == item))
+                    {
+                        var user = allUsers.Where(x => x.Id == item).FirstOrDefault();
+                        workersDTO.Add(mapper.Map<WorkerDTO>(user));
+                    }
+                }
+
+                if (workersDTO.Count > 0)
+                {
+                    return workersDTO;
+                }
+                else
+                {
+                    throw new ArgumentException("No workers available at this time");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Times not set appropriately");
+            }
+            
         }
     }
 }
