07198f057c5430b7a80d818913d6fc4686242085
Szabó Dárius <szabo.dariusz@gmail.com>
2021-11-17 20:07:14 +0100

    modified preview

--- wman-frontend/src/components/Print-functionality/Document-to-print/document-to-print.component.jsx
+++ wman-frontend/src/components/Print-functionality/Document-to-print/document-to-print.component.jsx
@@ -14,22 +14,35 @@ const DocumentToPrint = (props) => (
                 </div>
                 <div className='print-title'><h1>Events To Do:</h1></div>
                 <div className='print-all-events'>
-
-                {props.eventlist?.map(event =>
-                    <div className='one-event'>
-                        <br/>
-                        <h2>Event Details:</h2>
-                        <b>Desription:</b>{event.jobDescription}      Notes:<br/>
-                        <b>Start:</b>{event.estimatedStartDate}<br/>
-                        <b>Finish:</b>{event.estimatedFinishDate}
-                        <h3>Address:</h3>
-                        <b>City:</b>{event.address.city}<br/>
-                        <b>Street:</b>{event.address.street}<br/>
-                        <b>ZipCode:</b>{event.address.zipCode}<br/>
-                        <b>Building number:</b>{event.address.buildingNumber}<br/>
-                        <b>Floor door:</b>{event.address.floorDoor}<br/>                        
-                    </div>)}
+                    <div className='one-event-top'>
+                        <div className='one-event-title'>
+                            <h2>Event Details:</h2>
+                        </div>
+                        <div className='one-event-notes'>
+                            <h2>Notes:</h2>
+                        </div>
+                    </div>
+                    <ol>
+                        {props.eventlist?.map(event =>
+                            <li>
+                                <div className='one-event'>
+                                    <br />
+                                    <b>Desription: </b>{event.jobDescription}<br />
+                                    <b>Start: </b>{event.estimatedStartDate}<br />
+                                    <b>Finish: </b>{event.estimatedFinishDate}
+                                    <h3>Address: </h3>
+                                    <b>City: </b>{event.address.city}<br />
+                                    <b>Street: </b>{event.address.street}<br />
+                                    <b>ZipCode: </b>{event.address.zipCode}<br />
+                                    <b>Building number: </b>{event.address.buildingNumber}<br />
+                                    <b>Floor door: </b>{event.address.floorDoor}<br />
+                                </div>                                
+                            <hr/>
+                            </li>
+                            )}
+                    </ol>
                 </div>
+
 
                 <div className='pagenumber'>
 
