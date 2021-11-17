b1a689080c195c89940e45b3791ecc3037f80d21
Szabó Dárius <szabo.dariusz@gmail.com>
2021-11-17 20:06:01 +0100

    modified print-events component

--- wman-frontend/src/components/Print-functionality/Print-events/print-events.component.jsx
+++ wman-frontend/src/components/Print-functionality/Print-events/print-events.component.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { Cascader } from 'antd';
 import jwt_decode from 'jwt-decode';
 import axios from 'axios';
-import DocumentToPrint from '../To-print/document-to-print.component';
+import DocumentToPrint from '../Document-to-print/document-to-print.component';
 import Cookies from "js-cookie";
 import './print-events.styles.css';
 
 const PrintEvents = () => {
-
-    const [events, setEvents] = useState([]);
-    const [username, setUsername] = useState("");
     const [users, setUsers] = useState([]);
+
+    const [calendarEvents, setCalendarEvents] = useState([]);    
+    const [searched, setSearched] = useState("");
 
     useEffect(() => {
         axios.get(`/User/workload`)
@@ -18,29 +18,34 @@ const PrintEvents = () => {
             .catch(error => console.log(error));
     }, [axios]);
 
-    const handleChange = (e) => {
-        setUsername(e.target.value);
-        console.log(e.target.value);
-        axios.get(`/User/workEvents?username=${e.target.value}`)
-            .then(response => setEvents(response))
+    useEffect(() => {
+        axios.get(`/CalendarEvent/GetCurrentDayEvents`)
+            .then(response => setCalendarEvents(response.data))
             .catch(error => console.log(error))
+    }, [axios]);
+
+   const handleChange = (e) => {
+        setSearched(e.target.value);       
     }
-
-
+    
+    const filteredEvents = calendarEvents.filter(        
+        event => event.assignedUsers[0].username.includes(searched)
+    );
     return (
         <div className='print-events'>
+            <div className='print-events-prewiew'>
+                <DocumentToPrint eventlist={filteredEvents} />
+            </div>
             <div className='print-events-instructions'>
-                <br />                
                 <br />
+                <br />
+                <label>Choose the user!</label><br/>
                 <select onChange={handleChange} name="subject" id="subject">
-                    <option value="" selected disabled hidden>Choose the user!</option>
+                    <option value="" selected disabled hidden>Every worker</option>
                     {users.map(user =>
                         <option key={user.userID} value={user.username}>{user.username}</option>
                     )}
                 </select>
-            </div>
-            <div className='print-events-prewiew'>
-                <DocumentToPrint username={username} events={events}/>
             </div>
         </div>
     )
