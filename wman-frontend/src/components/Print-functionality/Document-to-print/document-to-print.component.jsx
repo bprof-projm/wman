590aad6dd0926c1212a5d30767b35af7495501f0
Szabó Dárius <szabo.dariusz@gmail.com>
2021-11-20 10:15:21 +0100

    Issue 2 fix: display no assigned events error message

--- wman-frontend/src/components/Print-functionality/Document-to-print/document-to-print.component.jsx
+++ wman-frontend/src/components/Print-functionality/Document-to-print/document-to-print.component.jsx
@@ -1,60 +1,68 @@
 import React from 'react';
 import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 import './document-to-print.stlyes.css'
+import { uuid } from 'uuidv4';
 
-const DocumentToPrint = (props) => (
 
-    <Document>
-        <Page size="A4">
-            <View>
-                <div className="print-header">
-                    <Text fixed>
-                        -created by WMAN application-
-                    </Text>
-                </div>
-                <div className='print-title'><h1>Events To Do:</h1></div>
-                <div className='print-all-events'>
-                    <div className='one-event-top'>
-                        <div className='one-event-title'>
-                            <h2>Event Details:</h2>
+
+const DocumentToPrint = (props) => {
+
+
+    const visible = (props.eventlist.length === 0);
+
+    return (
+        <Document>
+            <Page size="A4">
+                <View>
+                    <div className="print-header">
+                        <Text fixed>
+                            -created by WMAN application-
+                        </Text>
+                    </div>
+                    <div className='print-title'><h1>Events To Do:</h1></div>
+                    <div className='print-all-events'>
+                        <div className='one-event-top'>
+                            <div className='one-event-title'>
+                                <h2>Event Details:</h2>
+                            </div>
+                            <div className='one-event-notes'>
+                                <h2>Notes:</h2>
+                            </div>
                         </div>
-                        <div className='one-event-notes'>
-                            <h2>Notes:</h2>
+                        <ol>
+                            {props.eventlist?.map(event =>
+                                <li key={uuid()}>
+                                    <div className='one-event'>
+                                        <br />
+                                        <b>Desription: </b>{event.jobDescription}<br />
+                                        <b>Start: </b>{event.estimatedStartDate}<br />
+                                        <b>Finish: </b>{event.estimatedFinishDate}
+                                        <h3>Address: </h3>
+                                        <b>City: </b>{event.address.city}<br />
+                                        <b>Street: </b>{event.address.street}<br />
+                                        <b>ZipCode: </b>{event.address.zipCode}<br />
+                                        <b>Building number: </b>{event.address.buildingNumber}<br />
+                                        <b>Floor door: </b>{event.address.floorDoor}<br />
+                                    </div>
+                                    <hr />
+                                </li>
+                            )}
+                        </ol>
+                        <div className='preview-error'>
+                            {visible ? <h3>There are no events assigned to the chosen user or you didn't choose anybody!</h3> : null}
                         </div>
                     </div>
-                    <ol>
-                        {props.eventlist?.map(event =>
-                            <li>
-                                <div className='one-event'>
-                                    <br />
-                                    <b>Desription: </b>{event.jobDescription}<br />
-                                    <b>Start: </b>{event.estimatedStartDate}<br />
-                                    <b>Finish: </b>{event.estimatedFinishDate}
-                                    <h3>Address: </h3>
-                                    <b>City: </b>{event.address.city}<br />
-                                    <b>Street: </b>{event.address.street}<br />
-                                    <b>ZipCode: </b>{event.address.zipCode}<br />
-                                    <b>Building number: </b>{event.address.buildingNumber}<br />
-                                    <b>Floor door: </b>{event.address.floorDoor}<br />
-                                </div>                                
-                            <hr/>
-                            </li>
-                            )}
-                    </ol>
-                </div>
 
+                    <div className='pagenumber'>
 
-                <div className='pagenumber'>
-
-                    <Text render={({ pageNumber, totalPages }) => (
-                        `${pageNumber} / ${totalPages}`
-                    )} fixed />
-                </div>
-
-
-            </View>
-        </Page>
-    </Document>
-);
+                        <Text render={({ pageNumber, totalPages }) => (
+                            `${pageNumber} / ${totalPages}`
+                        )} fixed />
+                    </div>
+                </View>
+            </Page>
+        </Document>
+    )
+};
 
 export default DocumentToPrint;
