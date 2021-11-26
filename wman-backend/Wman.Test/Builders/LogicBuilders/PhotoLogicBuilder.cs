48ef15e21e0071afe7dd96db04b9ee75c6fbab4c
tubamark98 <val.afo00@gmail.com>
2021-11-26 10:50:13 +0100

    created the base PhotoLogicBuilder and Test

---
+++ wman-backend/Wman.Test/Builders/LogicBuilders/PhotoLogicBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using Wman.Data.DB_Models;
+using Wman.Repository.Interfaces;
+
+namespace Wman.Test.Builders.LogicBuilders
+{
+    class PhotoLogicBuilder
+    {
+        internal static List<Pictures> GetPictures()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Mock<IPicturesRepo> GetPicturesRepo(List<Pictures> pictureList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wman-backend/Wman.Test/Tests/PhotoLogicTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wman.Data.DB_Models;
+using Wman.Logic.Classes;
+using Wman.Logic.DTO_Models;
+using Wman.Repository.Interfaces;
+using Wman.Test.Builders;
+using Wman.Test.Builders.LogicBuilders;
+
+namespace Wman.Test.Tests
+{
+    class PhotoLogicTests
+    {
+        private IMapper mapper;
+
+        private Mock<UserManager<WmanUser>> userManager;
+        private List<WmanUser> users;
+
+        private Mock<IPicturesRepo> picturesRepo;
+        private List<Pictures> pictureList;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.users = UserManagerBuilder.GetWmanUsers();
+            this.userManager = UserManagerBuilder.GetUserManager(users);
+
+            this.mapper = MapperBuilder.GetMapper();
+
+            this.pictureList = PhotoLogicBuilder.GetPictures();
+            this.picturesRepo = PhotoLogicBuilder.GetPicturesRepo(pictureList);
+        }
+
+    }
+}
