d19465e3af772baa897c90bef2ca6ee1e33f350b
Szabó Dárius <szabo.dariusz@gmail.com>
2021-12-22 15:40:31 +0100

    add new route for workerpage

--- wman-frontend/src/App.js
+++ wman-frontend/src/App.js
@@ -7,11 +7,11 @@ import {
 } from "react-router-dom";
 
 import { NormalLoginForm } from "./components/Login/login.component.jsx"
-import { Logout } from "./components/Logout/logout.component.jsx";
 import Cookies from "js-cookie";
 import { TempPage } from "./components/DemoPage/temp.component.jsx";
 import PrintEvents from "./components/Print-functionality/Print-events/print-events.component.jsx";
 import EventDetails from "./components/eventDetails/eventDetails.jsx";
+import WorkerPage from "./components/Worker-page/worker-page.component.jsx";
 
 const axios = require("axios").default;
 
@@ -33,6 +33,7 @@ function App() {
         <Route path="/login" component={NormalLoginForm}/>  
         <ProtectedRoute path="/eventDetails" component={EventDetails} />
         <ProtectedRoute path="/print" component={PrintEvents}/>       
+        <ProtectedRoute path="/worker" component={WorkerPage} />
         <ProtectedRoute path="/" component={TempPage} />
       </Switch>
     </Router>
