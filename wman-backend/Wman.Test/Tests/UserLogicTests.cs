09688e01a46aed92f583ab4f4fc686688e6de24f
tubamark98 <val.afo00@gmail.com>
2021-12-29 20:18:49 +0100

    added 3 more tests into UserLogicTests

--- wman-backend/Wman.Test/Tests/UserLogicTests.cs
+++ wman-backend/Wman.Test/Tests/UserLogicTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -47,6 +48,7 @@ namespace Wman.Test.Tests
 
             //Assert
             Assert.That(call.ElementAt(2).Username == users[2].UserName);
+            
             this.userManager.Verify(x => x.Users, Times.Once);
             this.userManager.Verify(x => x.IsInRoleAsync(It.IsAny<WmanUser>(), It.IsAny<string>()), Times.Exactly(users.Count()));
         }
@@ -63,6 +65,7 @@ namespace Wman.Test.Tests
 
             //Assert
             Assert.That(call.ElementAt(1).Username == usernames[1]);
+            
             this.userManager.Verify(x => x.Users, Times.Once);
             this.userManager.Verify(x => x.IsInRoleAsync(It.IsAny<WmanUser>(), It.IsAny<string>()), Times.Exactly(usernames.Count()));
         }
@@ -79,7 +82,79 @@ namespace Wman.Test.Tests
 
             //Assert
             Assert.That(call.ElementAt(0).Id == users[0].WorkEvents.ElementAt(0).Id);
+            
             this.userManager.Verify(x => x.Users, Times.Once);
+            this.eventRepo.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Test]
+        public async Task WorkEventsOfLoggedInUser_ReturnsWorkEventProperly()
+        {
+            //Arrange
+            UserLogic userLogic = new(this.userManager.Object, this.mapper, this.eventRepo.Object);
+            string usernameToTest = users[0].UserName;
+
+            //Act
+            var call = await userLogic.WorkEventsOfLoggedInUser(usernameToTest);
+
+            //Assert
+            Assert.That(call.ElementAt(0).Id == users[0].WorkEvents.ElementAt(0).Id);
+            
+            this.userManager.Verify(x => x.Users, Times.Once);
+            this.eventRepo.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Test]
+        public async Task WorkEventsOfUserSpecific_ReturnsWorkEventProperly()
+        {
+            //Arrange
+            UserLogic userLogic = new(this.userManager.Object, this.mapper, this.eventRepo.Object);
+            string usernameToTest = users[0].UserName;
+            DateTime start = DateTime.UtcNow.AddDays(-1);
+            DateTime finish = DateTime.UtcNow.AddDays(1);
+
+            //Act
+            var call = await userLogic.WorkEventsOfUserSpecific(usernameToTest,start, finish);
+
+            //Assert
+            Assert.That(call.ElementAt(0).Id == users[0].WorkEvents.ElementAt(0).Id);
+
+            this.userManager.Verify(x => x.Users, Times.Once);
+            this.eventRepo.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Test]
+        public async Task WorkEventsOfUserToday_ReturnsWorkEventProperly()
+        {
+            //Arrange
+            UserLogic userLogic = new(this.userManager.Object, this.mapper, this.eventRepo.Object);
+            string usernameToTest = users[0].UserName;
+
+            //Act
+            var call = await userLogic.WorkEventsOfUserToday(usernameToTest);
+
+            //Assert
+            Assert.That(call.ElementAt(0).Id == users[0].WorkEvents.ElementAt(0).Id);
+
+            this.userManager.Verify(x => x.Users, Times.Once);
+            this.eventRepo.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Test]
+        public async Task GetEventDetailsForWorker_ReturnsWorkEventProperly()
+        {
+            //Arrange
+            UserLogic userLogic = new(this.userManager.Object, this.mapper, this.eventRepo.Object);
+            string usernameToTest = users[0].UserName;
+            int eventId = eventList[0].Id;
+
+            //Act
+            var call = await userLogic.GetEventDetailsForWorker(usernameToTest, eventId);
+
+            //Assert
+            Assert.That(call.Id == users[0].WorkEvents.ElementAt(0).Id);
+
+            this.eventRepo.Verify(x => x.GetOne(It.IsAny<int>()), Times.Once);
         }
     }
 }
