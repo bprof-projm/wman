4bad32e5393418e4ca8e3a4ee6f32cc27541473d
tubamark98 <val.afo00@gmail.com>
2022-01-03 16:10:54 +0100

    CreateWorkForce tested

--- wman-backend/Wman.Test/Tests/AdminLogicTests.cs
+++ wman-backend/Wman.Test/Tests/AdminLogicTests.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Identity;
 using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Wman.Data.DB_Models;
+using Wman.Logic.Classes;
+using Wman.Logic.DTO_Models;
 using Wman.Logic.Interfaces;
 using Wman.Logic.Services;
 using Wman.Repository.Interfaces;
@@ -28,6 +31,35 @@ namespace Wman.Test.Tests
             this.mapper = MapperBuilder.GetMapper();
 
             this.photoLogic = AdminLogicBuilder.PhotoLogicFactory(this.userManager.Object,this.mapper);
+        }
+
+        [Test]
+        public async Task CreateWorkForce_CreatesWorkerProperly()
+        {
+            //Arrange
+            AdminLogic adminLogic = new(this.userManager.Object, this.photoLogic, this.mapper);
+            RegisterDTO user = new()
+            {
+                Username = "fogvaratartottGyik",
+                Email = "maszkosfutocsiga@gmail.com",
+                Password = "miAR3dV2Sf0s",
+                Firstname = "Kronikus",
+                Lastname = "VeszettMacska",
+                PhoneNumber = "0690123456",
+                Role = "Worker"
+            };
+
+            //Act
+            var result = await adminLogic.CreateWorkforce(user);
+            
+            //Assert
+            Assert.True(result.Succeeded);
+            Assert.That(users.Count == 4);
+
+            this.userManager.Verify(x => x.Users, Times.Once);
+            this.userManager.Verify(x => x.CreateAsync(It.IsAny<WmanUser>(), It.IsAny<string>()), Times.Once);
+            this.userManager.Verify(x => x.AddToRoleAsync(It.IsAny<WmanUser>(), It.IsAny<string>()), Times.Once);
+
         }
     }
 }
