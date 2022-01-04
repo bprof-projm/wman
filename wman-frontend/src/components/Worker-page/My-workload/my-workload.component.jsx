e2d33e998b75feb3a579ce354259339a0f4abb64
Szabó Dárius <szabo.dariusz@gmail.com>
2022-01-04 21:23:15 +0100

    empty progress card

---
+++ wman-frontend/src/components/Worker-page/My-workload/my-workload.component.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import { Avatar, Progress } from "antd";
+
+const MyWorkload = () => {
+
+
+}
+
+export default MyWorkload;
--- wman-frontend/src/components/Worker-page/worker-page-today-events/worker-page-today.component.jsx
+++ wman-frontend/src/components/Worker-page/worker-page-today-events/worker-page-today.component.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { SmileTwoTone } from '@ant-design/icons';
 import "./worker-page-today.styles.css"
 import OneDayEvents from "../one-day-event/one-day-events.component";
+import ProgressCard from "../../Worker-load/Progress-card/progress-card.component";
 
 
 const WorkerToday = () => {
@@ -13,35 +14,44 @@ const WorkerToday = () => {
     const token = Cookies.get("auth");
     const decoded = jwt_decode(token);
     const [calendarEvents, setCalendarEvents] = useState([]);
+    const [workload, setWorkLoad] = useState();
+
+
+    useEffect(() => {
+        axios.get(`/Worker/myworkload`)
+            .then(response => setWorkLoad(response.data))
+            .catch(error => alert(error))
+    }, [axios]);
 
     useEffect(() => {
         axios.get(`/CalendarEvent/GetCurrentDayEvents`)
-            .then(response => setCalendarEvents(response.data))
-            .then(sort())
+            .then(response => setCalendarEvents(response.data))            
             .catch(error => alert(error))
     }, [axios]);
+
 
     const filteredEvents = calendarEvents.filter(
         event => event.assignedUsers.map(assigned => assigned.username).includes(decoded.sub));
 
     const showEmptyMessage = (filteredEvents.length === 0);
+    
 
-    function sort(){
-        if (showEmptyMessage === false) {
-
-            const today = new Date(filteredEvents[0].estimatedStartDate);
-            const dayName = today.toString().split(' ')[0];
-            console.log(dayName); //Mon,Tue,Wed,Thu,Fri,Sat,Sun
-        }
-    }
     return (
         <div>
             <div className="events-of-the-day">
+                {(showEmptyMessage)
+                    ? null
+                    : <ProgressCard
+                        key={workload.userID}
+                        src={workload.profilePicUrl}
+                        name={workload.username}
+                        percent={workload.percent} />                        
+                    }
+
                 <div className="one-day">
                     {showEmptyMessage
                         ? <div><h2>There are no jobs for today, go home! </h2> <br /> <h1><SmileTwoTone twoToneColor="lightgreen" /></h1></div>
                         : filteredEvents.map(oneEvent => <OneDayEvents key={oneEvent.id} event={oneEvent} />)}
-                        {sort()}
                 </div>
             </div>
         </div>
