ee2b467b9a04623c0732e1dde2ee1505220f90dd
Tamas Lengyel <lengyeltamas97@gmail.com>
2022-01-08 13:49:37 +0100

    Add initial calendar component

---
+++ wman-frontend/src/components/Manager/calendar-list.component.jsx
@@ -0,0 +1,211 @@
+import React, { Component, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import ColumnComponent from "./column.component.jsx";
+import { DragDropContext } from "react-beautiful-dnd";
+import styled from "styled-components";
+import { Layout, Menu } from "antd";
+import "./calendar-list.styles.css";
+
+const { Header, Content, Footer } = Layout;
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+`;
+
+class CalendarListComponent extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { initialData: initialData, currentWeek: [] };
+  // }
+  state = initialData;
+
+  onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      result.destination.droppableId === result.source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+    //TODO: order columns based on time
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      this.setState(newState);
+      return;
+    }
+
+    //Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+
+    this.setState(newState);
+  };
+
+  // generateListOfCurrentWeek() {
+  //   let curr = new Date();
+  //   let week = [];
+
+  //   for (let i = 1; i <= 7; i++) {
+  //     let first = curr.getDate() - curr.getDay() + i;
+  //     let day = new Date(curr.setDate(first)).toISOString().slice(0, 10);
+  //     week.push(day);
+  //   }
+
+  //   this.setState({ currentWeek: week });
+  // }
+
+  render() {
+    return (
+      <Layout>
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+          <div className="logo">Wman</div>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content
+          className="site-layout"
+          style={{ padding: "0 50px", marginTop: 64 }}
+        >
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 380 }}
+          >
+            <DragDropContext onDragEnd={this.onDragEnd}>
+              <Container>
+                {this.state.columnOrder.map((columnId) => {
+                  const column = this.state.columns[columnId];
+                  const tasks = column.taskIds.map(
+                    (taskId) => this.state.tasks[taskId]
+                  );
+
+                  return (
+                    <ColumnComponent
+                      key={column.id}
+                      column={column}
+                      tasks={tasks}
+                    />
+                  );
+                })}
+              </Container>
+            </DragDropContext>
+          </div>
+        </Content>
+        {/* <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer> */}
+      </Layout>
+    );
+  }
+}
+
+const initialData = {
+  tasks: {
+    "task-1": { id: "task-1", content: "task" },
+    "task-2": { id: "task-2", content: "task2" },
+    "task-3": { id: "task-3", content: "task3" },
+    "task-4": { id: "task-4", content: "task4" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "Monday",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"],
+    },
+
+    "column-2": {
+      id: "column-2",
+      title: "Tuesday",
+      taskIds: [],
+    },
+
+    "column-3": {
+      id: "column-3",
+      title: "Wednesday",
+      taskIds: [],
+    },
+
+    "column-4": {
+      id: "column-4",
+      title: "Thursday",
+      taskIds: [],
+    },
+
+    "column-5": {
+      id: "column-5",
+      title: "Friday",
+      taskIds: [],
+    },
+
+    "column-6": {
+      id: "column-6",
+      title: "Saturday",
+      taskIds: [],
+    },
+
+    "column-7": {
+      id: "column-7",
+      title: "Sunday",
+      taskIds: [],
+    },
+  },
+  columnOrder: [
+    "column-1",
+    "column-2",
+    "column-3",
+    "column-4",
+    "column-5",
+    "column-6",
+    "column-7",
+  ],
+};
+
+export default CalendarListComponent;
---
+++ wman-frontend/src/components/Manager/column.component.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Task from "./task.component.jsx";
+import { Droppable } from "react-beautiful-dnd";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgray;
+  border-radius: 2px;
+  width: calc(100% / 7);
+  background-color: ${(props) =>
+    props.droppableId === "column-6" ? "skyblue" : "lightgrey"};
+
+  dispaly: flex;
+  flex-direction: column;
+  height: 89vh;
+`;
+const Title = styled.h3`
+  padding: 8px;
+  text-align: center;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+  transition: background-color 0.2s ease;
+  background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "white")};
+  flex-grow: 1;
+  height: 83vh;
+`;
+
+class ColumnComponent extends Component {
+  render() {
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <Droppable droppableId={this.props.column.id}>
+          {(provided, snapshot) => (
+            <TaskList
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              isDraggingOver={snapshot.isDraggingOver}
+            >
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>
+    );
+  }
+}
+
+export default ColumnComponent;
