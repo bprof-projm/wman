3f527264c73a8424401880f38d68672986c25184
Luxeder Zoltán <luxederzoltan@gmail.com>
2022-03-13 18:57:26 +0100

    Created manager controller

---
+++ wman-backend/Wman.WebAPI/Controllers/ManagerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wman.Logic.Interfaces;
+
+namespace Wman.WebAPI.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Manager")]
+    public class ManagerController : ControllerBase
+    {
+        IEventLogic eventLogic;
+
+        public ManagerController(IEventLogic eventLogic)
+        {
+            this.eventLogic = eventLogic;
+
+        }
+        /// <summary>
+        /// asdf
+        /// </summary>
+        /// <returns>asd1</returns>
+        /// 
+        [HttpGet]
+        public async Task<ActionResult> test()
+        {
+            return Ok("asdf");
+        }
+    }
+}
