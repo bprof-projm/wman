e9ce43257579ddc6e368efa5d99f170e4a5257b8
Luxeder Zoltán <luxederzoltan@gmail.com>
2022-03-13 18:47:51 +0100

    Created xls model

---
+++ wman-backend/Wman.Logic/DTO_Models/ManagerXlsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wman.Logic.DTO_Models
+{
+    public class ManagerXlsModel
+    {
+        public string WorkerName { get; set; }
+        public string JobDesc { get; set; }
+        public AddressHUNDTO JobLocation { get; set; }
+        public DateTime JobStart { get; set; }
+        public DateTime JobEnd { get; set; }
+
+    }
+}
